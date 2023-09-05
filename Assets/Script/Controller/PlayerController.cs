0e84f39d73aca02019d726cf6aa68ee79a602853
Lee dohyun <12423098+riviea@users.noreply.github.com>
2023-09-05 21:35:55 +0900

    Add Scripts
    플레이어 이동 구현

---
+++ Assets/Script/Controller/PlayerController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    //float x = Input.GetAxis("Horizontal");
+    //    //float y = Input.GetAxis("Vertical");
+
+    //    //transform.position += (new Vector3(x, y).normalized*speed)*Time.deltaTime;
+    //}
+}
---
+++ Assets/Script/Entities/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputController _controller;
+    private Vector2 _movementDir = Vector2.zero;
+    private Rigidbody2D _rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<PlayerInputController>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        ApllyMovement(_movementDir);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDir = direction;
+    }
+
+    private void ApllyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        _rigidBody.velocity = direction;
+    }
+}
